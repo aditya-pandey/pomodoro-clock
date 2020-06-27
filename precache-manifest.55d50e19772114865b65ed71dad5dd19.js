b234b950eb226ff480cfcc6d26e71e7ddea7a24f
Aditya Pandey <adityadpandey@gmail.com>
2020-06-27 10:10:02 +0530

    Updates

---
+++ precache-manifest.55d50e19772114865b65ed71dad5dd19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9fb7c51466c4436b72d341ee7a14c68",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "31ced9ed776e07ab58f6",
+    "url": "/pomodoro-clock/static/css/main.df58c42e.chunk.css"
+  },
+  {
+    "revision": "c9cf4c27b7211e12371b",
+    "url": "/pomodoro-clock/static/js/2.6130551b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.6130551b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ced9ed776e07ab58f6",
+    "url": "/pomodoro-clock/static/js/main.6a254802.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
